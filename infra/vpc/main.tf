aac1088893fd247dac4751fcddc03e5883f4e9e6
leeplayed <leeplayed@naver.com>
2025-09-17 15:00:28 +0900

    Delete infra/vpc directory

--- infra/vpc/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "user12-vpc" {
-  cidr_block = var.cidr_block
-
-  tags = {
-    Name = "user12-vpc"
-  }
-}
--- infra/vpc/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type    = string
-  default = "ap-northeast-2"
-}
-
-variable "cidr_block" {
-  type    = string
-  default = "10.12.0.0/16"
-}
